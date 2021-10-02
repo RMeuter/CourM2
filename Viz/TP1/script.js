995e1e4bf45d332afd94f47db63b46ded0d1c266
RMeuter <36972352+RMeuter@users.noreply.github.com>
2021-10-02 14:02:24 +0200

    Move file of viz + Almost finish TP1

--- Viz/TP1/script.js
+++ Viz/TP1/script.js
@@ -10,37 +10,66 @@ function rand(mi, ma){
     return Math.random()*(ma-mi)+mi;
 }
 
-d3.json("miserables.json").then(function(graph) {
 
 
-    for(i=0;i<graph.nodes.length;i++){
-        var node = svg.append("circle"); // ajout d’un cercle
-        node.attr("id", "node"+graph.nodes[i].id); // identifiant
-        node.attr("r", rand(r)); 
-        node.attr("cx", rand(2*r, w-2*r)); // position X
-        node.attr("cy", rand(2*r, h-2*r)); // position y
-        node.attr("class", ".node"); // ajout d'une classe
-    }
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    for(let d = 0;  d < graph.links.length; d++){
-        var link = svg.append("line")
-        link.attr( "x1", d3.select("#node"+graph.links[d].source).attr("cx"));
-        link.attr( "y1", d3.select("#node"+graph.links[d].source).attr("cy"));
-        link.attr( "x2", d3.select("#node"+graph.links[d].target).attr("cx"));
-        link.attr( "y2", d3.select("#node"+graph.links[d].target).attr("cy"));
-        link.attr("stroke-width", 1).attr("stroke", "#aaaaaa"); // couleur de la bordure
-    }
 
-    for(i=0;i<graph.nodes.length;i++){
-        var node = svg.append("circle"); // ajout d’un cercle
-        node.attr("id", "node"+graph.nodes[i].id); // identifiant
-        node.attr("r", rand(r)); 
-        node.attr("cx", rand(2*r, w-2*r)); // position X
-        node.attr("cy", rand(2*r, h-2*r)); // position y
-        node.attr("class", ".node"); // ajout d'une classe
-    }
+d3.json("miserables.json").then(function(graph) {  
+    
+
+
+    // for(i=0;i<graph.nodes.length;i++){
+    //     var node = svg.append("circle"); // ajout d’un cercle
+    //     node.attr("r", r); 
+    //     node.attr("id", "node"+graph.nodes[i].id); // identifiant
+    //     // node.attr("cx", ); // position X
+    //     // node.attr("cy", ); // position y
+    //     node.attr("class", ".node"); // ajout d'une classe
+    //     node.attr("fill", function(d) { return color(graph.nodes[i].group) });
+    // }
 
     
-    //Creation de ligne des entre les ronds
+    var nodes = svg.selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("circle");
+    nodes.attr("stroke", "#aaaaaa")
+        .attr("r", 5)
+        .attr("id", function(d) { return "node"+d.id; } )
+        .attr("cx", function(d) { return rand(2*r, w-2*r); } )
+        .attr("cy", function(d) { return rand(2*r, h-2*r); } )
+        .attr("fill", function(d) { return color(d.group) });
+
+    var links = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line");
+        links.attr( "x1", function(d){ return d3.select("#node"+d.source).attr("cx");});
+        links.attr( "y1", function(d){ return d3.select("#node"+d.source).attr("cy");});
+        links.attr( "x2", function(d){ return d3.select("#node"+d.target).attr("cx");});
+        links.attr( "y2", function(d){ return d3.select("#node"+d.target).attr("cy");});
+        links.attr("stroke-width", function(d){ return Math.sqrt(d.value);})
+        links.style("opacity", 0.6);
+        links.attr("stroke", "#aaaaaa"); // couleur de la bordure
+
+
+    var forceDirectedLayout = d3.forceSimulation()
+    .nodes(graph.nodes)
+    .force("repulsion", d3.forceManyBody())
+    .force("attraction", d3.forceLink(graph.links))
+    .force("center", d3.forceCenter(w / 2, h / 2));
+
+    forceDirectedLayout.on("tick", function() {
+        links
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+        nodes
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+        }
+    );
+
 
 });
--- Viz/TP2/NoteTD.md
+++ Viz/TP2/NoteTD.md
@@ -1,5 +1,7 @@
 # TD1
 
+**Date :** 08 septembre 2021
+
 ## Exercice 1 :
 
 **Groupe ciblé:** Organisateur du salon de vin et l'entreprise Bacchuseo.
@@ -42,3 +44,24 @@ Etapes 4 : Conceptions des algo
     - Calculer complexité temps
     - Prouver que vos algo permet d'obtenir les résultat escomptés
 
+=> Reprise à 0
+
+## Exercice 5 :
+Générez des jeux de tests
+- Graphes synthétiques avec différentes structures (aléatoire, sans échelles, petit-monde, ...)
+- Graph synthétique des diff tailles (nombre de sommets et d'arretes)
+- graph réels
+
+Définissez des critère de qualité 
+- Distannce 2D entre u etv proportionnelle à la distance du plus court chemine entre u et v dans le graphes
+- Distance moyenne 2D intra cluster < Distance moyenne 2D intercluster
+
+Lancez votre algo de sur les diff graph et observez
+- le temps de calcul (est il assez rapide pour maintenir l'intéractivité de votre outil ?)
+- La qualité des résultats (via les criètre définis mais aussi via une analyse visuelle, au cas où les critères ne capturent pas toutes les propriétés du rendu final)
+
+
+## Exercice 6:
+Faire le "Carré latin" pour avoir une étude qui fait toutes les suites afin d'éviter les biais.
+
+=> 26'
